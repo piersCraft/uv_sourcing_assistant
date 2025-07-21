ae962fde0a6335eff268f19d7e6b8d63f10c14db
Piers Anderson <piers@craft.co>
2025-07-21 14:22:27 +0100

    initial commit with streamlit app boilerplate

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+# Chatbot setup
+
+st.title("Echo Bot")
+
+# Inititalise history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+def main():
+    print("Hello from uv-sourcing-assistant!")
+
+
+if __name__ == "__main__":
+    main()
