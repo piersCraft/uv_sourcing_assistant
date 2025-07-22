61f72100f49b7cb2253fcc13a6d050417082beb5
Piers Anderson <piers@craft.co>
2025-07-22 10:24:04 +0100

    added avatar images

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+from processors.agent import call_agent
 
 # Chatbot setup
-st.title("Echo Bot")
+favicon = 'craft_favicon.png'
+st.title("Craft Sourcing Assistant")
+st.logo(favicon,size="large")
+
+
+# TODO: Configure role avatars and styling
 
 # Inititalise history
 if "messages" not in st.session_state:
@@ -13,20 +19,18 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-if prompt := st.chat_input("What is up?"):
-    with st.chat_message("user"):
+if prompt := st.chat_input("Type your soucing query here"):
+    with st.chat_message("user", avatar="user_avatar_4.png"):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+    response = call_agent(prompt).output
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant",avatar="craft_favicon.png"):
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-
 
 
 def main():
--- processors/agent.py
+++ processors/agent.py
@@ -1,15 +1,29 @@
+import os
+from dotenv import load_dotenv
 from pydantic_ai import Agent
+
+# Variables if needed
+_ = load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise RuntimeError('Missing OPENAI_API_KEY in .env')
+
 # Agent setup
 agent = Agent(
-    'gpt-4.1',
+    model='gpt-4.1',
     system_prompt='Be concise, reply with one sentence',
+    output_type=str
 )
 
-result = agent.run_sync('Where does "hello world" come from?')
+def call_agent(user_input: str):
+    return agent.run_sync(f"{user_input}")  
+
+"""
+The first known use of "hello, world" was in a 1974 textbook about the C programming language.
+"""
 
 def main():
-    print(result.output)
-
+    print(call_agent("what is the capital of france").output)
 
 if __name__ == "__main__":
     main()
